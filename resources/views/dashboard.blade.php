6e91b571c090972ea735a0c2db94691297431c8e
Enrique Sousa <enrique.sousa@gmail.com>
2022-03-11 10:25:13 -0800

    Cambios a R-i7

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h2>Prueba en rama i7</h2>
+
+                <h1>Esta en la rama i7</h1>
+                <h2>Cambios, crear una lista ordenada R-i7</h2>
+                <ol>
+                    <li>Coffee</li>
+                    <li>Tea
+                        <ol>
+                            <li>Black tea</li>
+                            <li>Green tea</li>
+                        </ol>
+                    </li>
+                    <li>Milk</li>
+                </ol>
+
             </div>
         </div>
     </div>
