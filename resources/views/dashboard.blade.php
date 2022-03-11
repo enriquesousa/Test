5fd2f0f7a3db7ea449aab868122bbb650ee8b4df
Enrique Sousa <enrique.sousa@gmail.com>
2022-03-11 10:11:18 -0800

    new branch i5
    De una vez voy a crear una rama donde estare haciendo los cambios desde la maquina i5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <h1>Esta en la rama Master</h1>
-                
+                <h1>Esta en la rama i5</h1>
+
             </div>
         </div>
     </div>
